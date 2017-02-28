5cef4baef7b608921941a72efdd2ccb41be07518
Michael Berezanich <michaelberezanich@gmail.com>
2017-02-28 16:17:22 -0500

    Changed policy functions to sort functions, changed CPU assignment to support preemptive policies, added sort by priority function, hasn't been tested/debugged yet

--- Project1/myscheduler.cpp
+++ Project1/myscheduler.cpp
@@ -34,11 +34,11 @@ void MyScheduler::CreateThread(int arriving_time, int remaining_time, int priori
 }
 
 /****************************************************
-FCFS_policy: FIRST COME FIRST SERVE
 Sorts the CPU processes by their arrival time.
-Then schedules them by their position in the array.
+
+Used by FCFS_policy: FIRST COME FIRST SERVE
 ****************************************************/
-void MyScheduler::FCFS_policy(){ 
+void MyScheduler::SortbyAT(){ 
 
 	//insertion sort the arrays based on arrival time
 	//for all the elements in the threadVector
@@ -53,16 +53,17 @@ void MyScheduler::FCFS_policy(){
 	}
 
 	//at this point the vector should be sorted by arriving_time
+
+
 }
 
 /****************************************************
-STRFwoP_policy: SHORTEST TIME REMAINING WITHOUT PREEMPTION
-Sorts the CPU processes by their (remaining time-arrival time).
-Then schedules them by their position in the array.
+Sorts the CPU processes by their remaining time.
+
+Used by STRFwoP_policy: SHORTEST TIME REMAINING W/O PREEMPTION
+Used by STRFwP_policy: SHORTEST TIME REMAINING WITH PREEMPTION
 ****************************************************/
-void MyScheduler::STRFwoP_policy() {
-
-
+void MyScheduler::SortByRT() {
 
 	for (int i = 0; i < threadVector.size(); i++) { 
 		int j = i - 1;
@@ -73,21 +74,32 @@ void MyScheduler::STRFwoP_policy() {
 		}
 		threadVector.at(j + 1) = temp; 
 	}
-}
-
-/****************************************************
-STRFwP_policy: SHORTEST TIME REMAINING WITH PREEMPTION
-****************************************************/
-void MyScheduler::STRFwP_policy() {
 
 }
 
 /****************************************************
-PBS_policy: PRIORITY BASED SCHEDULING
 Sorts the CPU processes by their priority.
-Then schedules them by their position in the array.
+
+Used by PBS_policy: PRIORITY BASED SCHEDULING
 ****************************************************/
-void MyScheduler::PBS_policy() {
+void MyScheduler::SortByP() {
+
+	for (int i = 0; i < threadVector.size(); i++) {
+		int j = i - 1;
+		ThreadDescriptorBlock *temp = threadVector.at(i);
+		while (j >= 0 && temp->priority <= threadVector.at(j)->priority) {
+			if (temp->priority == threadVector.at(j)->priority) {
+				if (temp->arriving_time < threadVector.at(j)->arriving_time) {
+					threadVector.at(j + 1) = threadVector.at(j);
+				} //might need an else here to break out of loop?
+			}
+			else {
+				threadVector.at(j + 1) = threadVector.at(j);
+			}
+			j--;
+		}
+		threadVector.at(j + 1) = temp;
+	}
 
 }
 
@@ -118,12 +130,26 @@ int MyScheduler::FindNextThread() {
 	return -1;
 }
 
+bool MyScheduler::AssignThreadToCpu(ThreadDescriptorBlock *tcpu) {
+	int pos = FindNextThread();
+
+	//if FindNextThread finds a thread that needs assigned
+	if (pos != -1)
+		tcpu = threadVector.at(pos);
+	else
+		return false;
+
+	return true;
+}
+
+
+
 bool MyScheduler::Dispatch() {
 
 	//Remove completed threads from threadVector
 	for (int i = 0; i < threadVector.size(); i++) {
 		if (threadVector.at(i)->remaining_time < 1) {
-			//delete threadVector.at(i);
+			//delete threadVector.at(i); or &threadVector.at(i); ? Do we need one of these or will erase do this
 			threadVector.erase(threadVector.begin() + i);
 		}
 	}
@@ -135,16 +161,14 @@ bool MyScheduler::Dispatch() {
 	switch (policy)
 	{
 	case FCFS:		//First Come First Serve
-		FCFS_policy();
-		break;
-	case STRFwoP:	//Shortest Time Remaining First, without preemption
-		STRFwoP_policy();
-		break;
-	case STRFwP:	//Shortest Time Remaining First, with preemption
-		STRFwP_policy();
+		SortbyAT();
 		break;
 	case PBS:		//Priority Based Scheduling, with preemption
-		PBS_policy();
+		SortByP();
+		break;
+	case STRFwoP:	//Shortest Time Remaining First, without preemption
+	case STRFwP:	//Shortest Time Remaining First, with preemption
+		SortByRT(); //Both use SortByRT()
 		break;
 	default:
 		cout << "Invalid policy!";
@@ -157,15 +181,29 @@ bool MyScheduler::Dispatch() {
 	//If FindNextThread() call returns with error, no threads can be scheduled yet so we escape
 	for (int i = 0; i < num_cpu && threadsAvailable; i++) {
 
-		if (CPUs[i] == NULL) {
-			int pos = FindNextThread();
-
-			//if FindNextThread finds a thread that needs assigned
-			if (pos != -1) 
-				CPUs[i] = threadVector.at(pos);
-			else 
-				threadsAvailable = false;
+		//For non preemptive policies, wait until the next cpu is null
+		if (policy == FCFS || policy == STRFwoP) {
+			if (CPUs[i] == NULL) {
+				threadsAvailable = AssignThreadToCpu(CPUs[i]);
+			}
 		}
+		else if (policy == STRFwP) {
+			ThreadDescriptorBlock temp;
+			threadsAvailable = AssignThreadToCpu(&temp);
+
+			if (threadsAvailable && temp.remaining_time < CPUs[i]->remaining_time) {
+				threadsAvailable = AssignThreadToCpu(CPUs[i]);
+			}
+		}
+		else if (policy == PBS) {
+			ThreadDescriptorBlock temp;
+			threadsAvailable = AssignThreadToCpu(&temp);
+
+			if (threadsAvailable && temp.priority < CPUs[i]->priority) {
+				threadsAvailable = AssignThreadToCpu(CPUs[i]);
+			}
+		}
+
 	}
 	
 
--- Project1/myscheduler.h
+++ Project1/myscheduler.h
@@ -36,12 +36,10 @@ public:
 	bool Dispatch() override; //Function to implement scheduling policy and to keep a check on processed threads
 	void CreateThread(int arriving_time, int remaining_time, int priority, int tid) override; //Function to create threads and insert them in student defined data structure
 	
-	int MyScheduler::FindNextThread();
+	int FindNextThread();
+	bool AssignThreadToCpu(ThreadDescriptorBlock *tcpu);
 
-	void FCFS_policy();
-	void STRFwoP_policy();
-	void STRFwP_policy();
-	void PBS_policy();
-
-
+	void SortbyAT();
+	void SortByRT();
+	void SortByP();
 };
